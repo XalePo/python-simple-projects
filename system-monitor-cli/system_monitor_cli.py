0a82d72c6ae5c8b88a01133743e12dd173956877
Alejandro <alemperez24@gmail.com>
2026-05-15 23:42:44 -0400

    Refactor back to dict to store the data and finish implementation of remaining functions

--- system-monitor-cli/system_monitor_cli.py
+++ system-monitor-cli/system_monitor_cli.py
@@ -1,48 +1,95 @@
+from typing import Dict, Tuple
 import psutil
-from typing import Tuple
+import time
 
 
 def main():
-    cpu_usage = get_cpu_usage()
-    mem_total, mem_available, mem_used, mem_free, mem_perc = get_memory_usage()
-    disk_total, disk_used, disk_free, disk_perc = get_disk_usage()
-    battery_perc = get_battery_percentage()
-    sys_uptime = get_sys_uptime()
+    sys_info = {
+        "cpu": get_cpu_usage(),
+        "memory": get_memory_usage(),
+        "disk": get_disk_usage(),
+        "battery": get_battery_percentage(),
+        "sys_uptime": get_sys_uptime(),
+    }
     
-    print()
+    display_report(sys_info)
 
 
-def get_cpu_usage():
-    return f"{psutil.cpu_percent(interval=0.1)}"
+def get_cpu_usage() -> float:
+    return f"{psutil.cpu_percent(interval=1)}"
 
 
-def get_memory_usage():
+def get_memory_usage() -> Dict:
     mem = psutil.virtual_memory()
     
-    return (*map(bytes_to_gigabytes, (mem.total, mem.available, mem.used, mem.free)), mem.percent)
+    mem_total, mem_available, mem_used, mem_free = tuple(map(bytes_to_gigabytes, (mem.total, mem.available, mem.used, mem.free)))
+
+    return {
+        "total": mem_total,
+        "available": mem_available,
+        "percent": mem.percent,
+        "used": mem_used,
+        "free": mem_free,
+    }
 
 
-def get_disk_usage():
+def get_disk_usage() -> Dict:
     disk = psutil.disk_usage("/System/Volumes/Data")
-    print(disk)
-    
-    return (*map(bytes_to_gigabytes, (disk.total, disk.used, disk.free)), disk.percent)
+
+    disk_total, disk_used, disk_free = tuple(map(bytes_to_gigabytes, (disk.total, disk.used, disk.free)))
+
+    return {
+        "total": disk_total,
+        "used": disk_used,
+        "free": disk_free,
+        "percent": disk.percent,
+    }
 
 
-def get_battery_percentage():
-    pass
+def get_battery_percentage() -> int:
+    battery = psutil.sensors_battery()
+
+    return battery.percent
 
 
-def get_sys_uptime():
-    pass
+def get_sys_uptime() -> Dict:
+    boot_time = psutil.boot_time()
+    current_time = time.time()
+    uptime_sec = current_time - boot_time
+
+    hours = int(uptime_sec // 3600)
+    minutes = int((uptime_sec % 3600) // 60)
+
+    return {
+        "hours": hours,
+        "minutes": minutes,
+    }
 
 
 def bytes_to_gigabytes(byte_val) -> Tuple[float, ...]:
         return round(byte_val / 1073741824, 2)
 
 
-def display_report(system_info) -> None:
-    pass
+def display_report(sys_info) -> None:
+    print("Sytem Monitoring Report")
+    print("-----------------------")
+    print(f"CPU Usage: {sys_info["cpu"]}%")
+
+    print("\nMemory:")
+    print(f"Total: {sys_info["memory"]["total"]} GB")
+    print(f"Used: {sys_info["memory"]["used"]} GB")
+    print(f"Available: {sys_info["memory"]["available"]} GB")
+    print(f"Free: {sys_info["memory"]["free"]} GB")
+    print(f"Usage: {sys_info["memory"]["percent"]}%")
+
+    print("\nDisk")
+    print(f"Total: {sys_info["disk"]["total"]} GB")
+    print(f"Used: {sys_info["disk"]["used"]} GB")
+    print(f"Free: {sys_info["disk"]["free"]} GB")
+    print(f"Usage: {sys_info["disk"]["percent"]}")
+
+    print(f"\nBattery: {sys_info["battery"]}%")
+    print(f"System Uptime: {sys_info["sys_uptime"]["hours"]} hours and {sys_info["sys_uptime"]["minutes"]} minutes")
 
 
 if __name__ == "__main__":
